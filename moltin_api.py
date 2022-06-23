c5b63c440abe42174dd93a6c9136c6b608e18d64
Aosothra <arhdam@yandex.ru>
2022-06-23 23:39:00 +0300

    initial commit

---
+++ moltin_api.py
@@ -0,0 +1,129 @@
+import json
+import requests
+from environs import Env
+
+
+class SimpleMoltinApiClient:
+    def __init__(self, client_id, client_secret):
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+        self.__access_token = None
+
+
+    def __get_access_token(self, update=False):
+        """Get or retrieve access token. Update flag will force retrieval"""
+
+        if self.__access_token and not update:
+            return self.__access_token
+
+        url = "https://api.moltin.com/oauth/access_token"
+        data = {
+            "client_id": self.__client_id,
+            "client_secret": self.__client_secret,
+            "grant_type": "client_credentials"
+        }
+
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+        auth_data = response.json()
+
+        self.__access_token = auth_data["access_token"]
+
+        return self.__access_token
+
+
+    def __raw_api_call(self, method, query, **kwargs):
+        """Make an API call with provided parameters"""
+
+        url = f"https://api.moltin.com/v2/{query}"
+        headers = {
+            "Authorization": f"Bearer {self.__get_access_token()}"
+        }
+        if kwargs is not None:
+            headers["Content-Type"] = "application/json"
+        response = requests.request(method, url, headers=headers, json={"data": kwargs})
+
+        if response.status_code == 401:
+            headers["Authorization"] = (
+                f"Bearer {self.__get_access_token(update=True)}"
+            )
+
+            response = requests.request(method, url, headers=headers, json={"data": kwargs})
+            response.raise_for_status()
+        
+        try:
+            return response.json()
+        except ValueError:
+            return None
+
+
+    def get_products(self):
+        product_data = self.__raw_api_call("GET", "products")
+        return {
+            product["name"]: product["id"] 
+            for product in product_data["data"]
+        }
+
+    
+    def get_product_by_id(self, id):
+        product_info = self.__raw_api_call("GET", f"products/{id}")
+        return product_info["data"]
+
+    
+    def get_image_url_by_file_id(self, id):
+        file_info = self.__raw_api_call("GET", f"files/{id}")
+        return file_info["data"]["link"]["href"]
+
+
+    def remove_product_from_cart(self, cart_id, item_id):
+        self.__raw_api_call("DELETE", f"carts/{cart_id}/items/{item_id}")
+
+    
+    def get_cart_and_full_price(self, cart_id):
+        items_info = self.__raw_api_call("GET", f"carts/{cart_id}/items")
+        return (
+            items_info["data"], 
+            items_info["meta"]["display_price"]["with_tax"]["formatted"]
+        )
+
+    
+    def add_product_to_cart(self, cart_id, product_id, quantity):
+        self.__raw_api_call("POST", f"carts/{cart_id}/items", 
+            id=product_id,
+            type="cart_item",
+            quantity=quantity
+        )
+
+    
+    def get_or_create_customer_by_email(self, email):
+        customer_info = self.__raw_api_call("GET", f"customers?filter=eq(email,{email})")
+        if customer_info["data"]:
+            return customer_info["data"][0]["id"]
+
+        customer_info = self.__raw_api_call("POST", "customers",
+            type="customer",
+            name="Anonymous Customer",
+            email=email
+        )
+
+        return customer_info["data"]["id"]
+
+
+    def empty_cart(self, cart_id):
+        self.__raw_api_call("DELETE", f"cart/{cart_id}")
+
+
+    def checkout(self, cart_id, customer_id):
+        placeholder_data = {
+                "first_name": "na",
+                "last_name": "na",
+                "line_1": "na",
+                "region": "na",
+                "postcode": "na",
+                "country": "na"
+            }
+        self.__raw_api_call("POST", f"carts/{cart_id}/checkout",
+            customer={"id":customer_id},
+            billing_address=placeholder_data,
+            shipping_address=placeholder_data
+        )
---
+++ tg_bot.py
@@ -0,0 +1,290 @@
+import os
+import re
+from environs import Env
+import redis
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Filters, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+from moltin_api import SimpleMoltinApiClient
+
+START, HANDLE_MENU, HANDLE_DESCRIPTION, HANDLE_CART, HANDLE_EMAIL = range(5)
+
+_database = None
+_moltin_api = None
+
+def start(update, context):
+    text = (
+        "Welcome to out humble fish market.\n"
+        "What product are you interested in?"
+    )
+
+    product_keyboard = [
+        [InlineKeyboardButton(product_name, callback_data=product_id)]
+        for product_name, product_id 
+        in get_moltin_client().get_products().items()
+    ]
+    
+    update.message.reply_text(
+        text=text,
+        reply_markup=InlineKeyboardMarkup(product_keyboard)
+    )
+
+    return HANDLE_MENU
+
+
+def handle_menu(update, context):
+    if not update.callback_query:
+        return HANDLE_MENU
+
+    users_reply = update.callback_query.data
+    update.callback_query.delete_message()
+
+    product_info = get_moltin_client().get_product_by_id(users_reply)
+    image_url = get_moltin_client().get_image_url_by_file_id(
+        product_info["relationships"]["main_image"]["data"]["id"]
+    )
+
+    text = (
+        f"{product_info['name']}\n\n"
+        f"{product_info['meta']['display_price']['with_tax']['formatted']} per kg\n"
+        f"{product_info['meta']['stock']['level']} kg in stock"
+    )
+
+    options_keyboard = [
+        [
+            InlineKeyboardButton("1 kg", callback_data=f"{product_info['id']}:1"),
+            InlineKeyboardButton("5 kg", callback_data=f"{product_info['id']}:5"),
+            InlineKeyboardButton("10 kg", callback_data=f"{product_info['id']}:10")
+        ],
+        [InlineKeyboardButton("Back", callback_data="return")],
+        [InlineKeyboardButton("Cart", callback_data="cart")]
+    ]
+
+    update.callback_query.message.reply_photo(
+        image_url,
+        caption=text,
+        reply_markup=InlineKeyboardMarkup(options_keyboard)
+    )
+
+    return HANDLE_DESCRIPTION
+
+
+def get_text_and_buttons_for_cart(cart_id):
+    cart_items, full_price = get_moltin_client().get_cart_and_full_price(cart_id)
+    cart_items_display = [
+        f"{item['name']}\n"
+        f"{item['meta']['display_price']['with_tax']['unit']['formatted']} per kg\n"
+        f"{item['quantity']}kg for "
+        f"{item['meta']['display_price']['with_tax']['value']['formatted']}"
+        for item in cart_items
+    ]
+    cart_items_display.append(
+        f"Total price: {full_price}"
+    )
+
+    text = (
+        "\n\n".join(cart_items_display)
+        if cart_items else
+        "Cart is empty. Go on and throw in some stuff :D"
+    )
+
+    cart_keyboard = [
+        [InlineKeyboardButton(f"Remove {item['name']}", callback_data=item["id"])]
+        for item in cart_items 
+    ]
+    cart_keyboard.append(
+        [InlineKeyboardButton(f"Back", callback_data="return")]
+    )
+    if cart_items:
+        cart_keyboard.append(
+            [InlineKeyboardButton(f"Checkout", callback_data="checkout")]
+        )
+
+    return text, cart_keyboard
+
+
+def handle_description(update, context):
+    if not update.callback_query:
+        return HANDLE_DESCRIPTION
+
+    users_reply = update.callback_query.data
+    if users_reply == "cart":
+        update.callback_query.edit_message_reply_markup(reply_markup=None)
+        
+        text, cart_keyboard = get_text_and_buttons_for_cart(update.effective_chat.id)
+
+        update.callback_query.message.reply_text(
+            text=text,
+            reply_markup = InlineKeyboardMarkup(cart_keyboard)
+        )
+        return HANDLE_CART
+
+    if users_reply == "return":
+        update.callback_query.edit_message_reply_markup(reply_markup=None)
+
+        product_keyboard = [
+            [InlineKeyboardButton(product_name, callback_data=product_id)]
+            for product_name, product_id 
+            in get_moltin_client().get_products().items()
+        ]
+
+        update.callback_query.message.reply_text(
+            text="What product are you interested in?",
+            reply_markup=InlineKeyboardMarkup(product_keyboard)
+        )
+        return HANDLE_MENU
+    
+    product_id, quantity = users_reply.split(":")
+    get_moltin_client().add_product_to_cart(
+        cart_id=update.effective_chat.id,
+        product_id=product_id,
+        quantity=int(quantity)
+    )
+    update.callback_query.answer(text="Item added to the cart")
+
+    return HANDLE_DESCRIPTION
+
+
+def handle_cart(update, context):
+    if not update.callback_query:
+        return HANDLE_CART
+
+    users_reply = update.callback_query.data
+
+    if users_reply == "return":
+        update.callback_query.edit_message_reply_markup(reply_markup=None)
+
+        product_keyboard = [
+            [InlineKeyboardButton(product_name, callback_data=product_id)]
+            for product_name, product_id 
+            in get_moltin_client().get_products().items()
+        ]
+
+        update.callback_query.message.reply_text(
+            text="What product are you interested in?",
+            reply_markup=InlineKeyboardMarkup(product_keyboard)
+        )
+
+        return HANDLE_MENU
+    
+    if users_reply == "checkout":
+        update.callback_query.edit_message_reply_markup(reply_markup=None)
+
+        text = (
+            "To complete your order please enter your email address.\n"
+            "Our commercial department will issue the payment."
+        )
+
+        update.callback_query.message.reply_text(
+            text=text,
+        )
+        return HANDLE_EMAIL
+
+    get_moltin_client().remove_product_from_cart(
+        cart_id=update.effective_chat.id,
+        item_id=users_reply
+    )
+
+    text, cart_keyboard = get_text_and_buttons_for_cart(update.effective_chat.id)
+
+    update.callback_query.answer(text="Item removed from the cart")
+    update.callback_query.edit_message_text(
+        text=text,
+        reply_markup=InlineKeyboardMarkup(cart_keyboard)
+    )
+
+    return HANDLE_CART
+
+
+def handle_email(update, context):
+    user_reply = update.message.text
+    if not re.match(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', user_reply.lower()):
+        update.message.reply_text(
+            text="Your email look kinda messed. Please try again.",
+        )
+        return HANDLE_EMAIL
+    
+    customer_id = get_moltin_client().get_or_create_customer_by_email(user_reply)
+    get_moltin_client().checkout(update.effective_chat.id, customer_id)
+    get_moltin_client().empty_cart(update.effective_chat.id)
+
+    update.message.reply_text(
+        text="Thank you for your order! We will contact you at once :D",
+    )
+
+    return START
+
+
+def handle_users_reply(update, context):
+    """Handle by state machine"""
+
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = START
+    else:
+        user_state = int(db.get(chat_id))
+    
+    states_functions = {
+        START: start,
+        HANDLE_MENU: handle_menu,
+        HANDLE_DESCRIPTION: handle_description,
+        HANDLE_CART: handle_cart,
+        HANDLE_EMAIL: handle_email,
+    }
+    state_handler = states_functions[user_state]
+
+    next_state = state_handler(update, context)
+    db.set(chat_id, next_state)
+
+
+def get_database_connection():
+    """Create or retrive connection to redis database"""
+    global _database
+    if _database is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+
+        _database = redis.Redis(
+            host=database_host, 
+            port=database_port, 
+            password=database_password, 
+            decode_responses=True
+        )
+
+    return _database
+
+
+def get_moltin_client():
+    """Get or create instance of Moltin API Client to maintain access token"""
+    global _moltin_api
+    if _moltin_api is None:
+        client_id = os.getenv("CLIENT_ID")
+        client_secret = os.getenv("CLIENT_SECRET")
+        _moltin_api = SimpleMoltinApiClient(client_id, client_secret)
+
+    return _moltin_api
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    
+    token = env("BOT_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    updater.start_polling()
+    updater.idle()
